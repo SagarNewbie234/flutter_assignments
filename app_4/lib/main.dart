d752d05bef5e337794cd64eb64d16407643370c0
SagarNewbie234 <sagarbhattacharyamain@gmail.com>
2025-01-16 20:33:11 +0530

    commit 5

--- app_4/lib/main.dart
+++ app_4/lib/main.dart
@@ -1,33 +1,96 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StopWatchApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StopWatchApp extends StatelessWidget {
+  const StopWatchApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const MyHomePage(),
+      title: 'Assignment 04',
+      home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  late Stopwatch stopwatch;
+  late Timer timer;
+
+  String returnFormattedText() {
+    var milli = stopwatch.elapsedMilliseconds;
+    String milliseconds = (milli % 100).toString().padLeft(2, "0");
+    String seconds = ((milli ~/ 1000) % 60).toString().padLeft(2, "0");
+    String minutes = ((milli ~/ 1000) ~/ 60).toString().padLeft(2, "0");
+
+    return "$minutes:$seconds:$milliseconds";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    stopwatch = Stopwatch();
+    timer =
+        Timer.periodic(Duration(milliseconds: 60), (timer) => setState(() {}));
+  }
+
+  Widget buildActionButtons(String buttonText, VoidCallback onPressed) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FilledButton(
+        onPressed: onPressed,
+        style: FilledButton.styleFrom(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        ),
+        child: Text(buttonText),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "StopWatch",
+          style: GoogleFonts.fredoka(),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                returnFormattedText(),
+                style: GoogleFonts.roboto(fontSize: 50),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildActionButtons("Start", stopwatch.start),
+                buildActionButtons("Stop", stopwatch.stop),
+                buildActionButtons("Reset", stopwatch.reset),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
