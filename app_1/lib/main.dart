d870584c6ba5e89b0b40ae5b971429fb4efad06e
SagarNewbie234 <sagarbhattacharyamain@gmail.com>
2025-01-16 19:31:56 +0530

    commit 1

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,134 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Assignment 01',
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController tec = TextEditingController();
+  TextEditingController inputUnit = TextEditingController();
+  TextEditingController outputUnit = TextEditingController();
+  String displayText = "0.0";
 
-  void _incrementCounter() {
+  void convert() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      double inputValue = double.parse(tec.text);
+
+      Map<String, Map<String, Function(double)>> conversionMap = {
+        "Celsius": {
+          "Celsius": (temp) => temp,
+          "Fahrenheit": (temp) => (temp * 1.8) + 32,
+          "Kelvin": (temp) => temp + 273.15,
+        },
+        "Fahrenheit": {
+          "Celsius": (temp) => (temp - 32) * (5 / 9),
+          "Fahrenheit": (temp) => temp,
+          "Kelvin": (temp) => ((temp - 32) * (5 / 9)) + 273.15,
+        },
+        "Kelvin": {
+          "Celsius": (temp) => temp - 273.15,
+          "Fahrenheit": (temp) => ((temp - 273.15) * 1.8) + 32,
+          "Kelvin": (temp) => temp,
+        },
+      };
+
+      if (conversionMap.containsKey(inputUnit.text) &&
+          conversionMap[inputUnit.text]!.containsKey(outputUnit.text)) {
+        double result =
+            conversionMap[inputUnit.text]![outputUnit.text]!(inputValue);
+        displayText =
+            "${inputValue.toStringAsFixed(2)} ${inputUnit.text[0]} = ${result.toStringAsFixed(2)} ${outputUnit.text[0]}";
+      } else {
+        displayText = "Invalid Input";
+      }
     });
+
+    tec.clear();
+    inputUnit.clear();
+    outputUnit.clear();
+  }
+
+  Widget buildDropDownButton(String helperText, TextEditingController tec) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6.0),
+      child: DropdownMenu(
+        dropdownMenuEntries: ["Celsius", "Fahrenheit", "Kelvin"].map((element) {
+          return DropdownMenuEntry(value: element, label: element);
+        }).toList(),
+        width: double.maxFinite,
+        helperText: helperText,
+        controller: tec,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Assignment - 01", style: GoogleFonts.fredoka()),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                (displayText == "0.0") ? "" : "Conversion Result",
+                style: GoogleFonts.fredoka(fontSize: 24),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  displayText,
+                  style: GoogleFonts.fredoka(fontSize: 35),
+                  textAlign: TextAlign.end,
+                ),
+              ),
+              Container(
+                width: double.maxFinite,
+                padding: EdgeInsets.symmetric(vertical: 12.0),
+                child: TextField(
+                  controller: tec,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Temperature",
+                  ),
+                  focusNode: FocusNode(canRequestFocus: false),
+                ),
+              ),
+              buildDropDownButton("Input Unit", inputUnit),
+              buildDropDownButton("Output Unit", outputUnit),
+              SizedBox(height: 20),
+              FilledButton(
+                onPressed: convert,
+                style: FilledButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: Text("Convert Temperature"),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
